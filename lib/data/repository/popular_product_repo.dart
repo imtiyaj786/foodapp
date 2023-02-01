479b0b7995ed89851600ce2717ab3c20fafa55ad
imtiyaj786 <imtiyaj1523@gmail.com>
2023-02-01 19:04:10 +0530

    apiClient added not done

---
+++ lib/data/repository/popular_product_repo.dart
@@ -0,0 +1,11 @@
+import 'package:foodapp/data/api/api_client.dart';
+import 'package:get/get.dart';
+
+class PopularProductRepo extends GetxService {
+  final ApiClient apiClient;
+
+  PopularProductRepo({required this.apiClient});
+  Future<Response> getPopularProductList() async {
+    return await apiClient.getData("https://www.dbestech.com/api/product/list");
+  }
+}
